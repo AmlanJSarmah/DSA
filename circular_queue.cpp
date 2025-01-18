deb9bb819ae652cdca6bd36ea287fc2574e1f025
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2025-01-18 23:00:26 +0530

    Added documentation for circular queue

--- circular_queue.cpp
+++ circular_queue.cpp
@@ -16,6 +16,7 @@ int CircularQueue::get_front() {
 int CircularQueue::get_rear() {
 	if (size == 0)
 		return INT_MIN;
+	//We find the last element. (front + size - 1) % capacity returns the last element with a value
 	int rear = (front + size - 1) % capacity;
 	return data[rear];
 }
@@ -24,6 +25,7 @@ void CircularQueue::enqueue(int value)
 {
 	if (size == capacity)
 		return;
+	//We find the first free index and insert a new value. (front + size) % capacity returns the first index where we can insert a value
 	int rear = (front + size) % capacity;
 	data[rear] = value;
 	size++;
@@ -34,6 +36,7 @@ int CircularQueue::dequeue()
 	if (size == -1)
 		return INT_MIN;
 	int item = data[front];
+	//We move the front to next index, hence the previous front becomes free to use. Hence, we decrement the size
 	front = (front + 1) % capacity;
 	size--;
 	return item;
